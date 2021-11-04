da11a7c0b4c277a3d87bba2353a4cd493931998c
maitriyogin <stephen.white@mac.com>
2021-11-04 09:36:36 +0100

    Upadte readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+import { useAtom } from "jotai";
+import { Count1 } from "./Count1";
+import { Count2 } from "./Count2";
+import { Count3, CountTotal } from "./Count3";
+import { totalCount } from "./count-atom";
 function App() {
+  // const tc = useAtom(totalCount);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Total Count : <CountTotal />
       </header>
+      <div className="Count-Container">
+        <Count3 />
+      </div>
     </div>
   );
 }
